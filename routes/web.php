4971bf3665e767dfa1f058b67f0be22f86c80e05
JeroenBolhuis <j.bolhuis@student.avans.nl>
2025-02-13 17:01:48 +0100

    removed unused routes

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 // Public routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
-Route::get('/privacy', [HomeController::class, 'privacy'])->name('privacy');
-Route::get('/terms', [HomeController::class, 'terms'])->name('terms');
 
 // Listing routes
 Route::get('/listings', [ListingController::class, 'index'])->name('listings.index');
