05ef36c2b0cd9fb085332d3ab532d5a2aa490aab
JeroenBolhuis <j.bolhuis@student.avans.nl>
2025-02-13 17:35:36 +0100

    better layouts for phones

--- resources/views/auctions/index.blade.php
+++ resources/views/auctions/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-6 lg:px-8">
         <!-- Header -->
         <div class="mb-8">
             <h2 class="text-2xl font-bold text-gray-900 dark:text-gray-100">{{ __('Auctions') }}</h2>
