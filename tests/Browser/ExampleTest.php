ec8e9666637adc61c016c2753fdf9c4a2b7eec81
Anton Jagers <36148924+adjagers@users.noreply.github.com>
2025-04-06 18:55:48 +0200

    adding some extra setup such of the database configurations will be wrong otherwise

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,17 +5,40 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 class ExampleTest extends DuskTestCase
 {
-    /**
-     * A basic browser test example.
-     */
+    use DatabaseMigrations;
+
     public function testBasicExample(): void
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Je');
+                ->assertSee('Je');
+
         });
     }
+
+    /** @test */
+    public function user_can_register_without_advertisement_access()
+    {
+        Artisan::call('db:seed');
+
+        $email = 'testuser@example.com';
+
+        $this->browse(function (Browser $browser) use ($email) {
+            $browser->visit(route('register'))
+                ->type('name', 'Test User')
+                ->type('email', $email)
+                ->type('password', 'Jagoed123!')
+                ->type('password_confirmation', 'Jagoed123!')
+                ->press('REGISTREREN')
+                ->pause(1000)
+                ->screenshot('after-registration')
+                ->assertPathIs('/dashboard')
+                ->visit(route('advertisements.create'));
+        });
+    }
+
 }
